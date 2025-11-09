1deccbc74da884661c82f3480441c436ac0a1075
Klemen <klemen.nagode@gmail.com>
2025-11-09 19:28:22 +0100

    simplify config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
-
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger]
-  config.dsn = ENV['SENTRY_DSN']
-  config.traces_sample_rate = 1.0
+if ENV['SENTRY_DSN'].present?
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.traces_sample_rate = 1.0
+    config.send_default_pii = true
+  end
 end
